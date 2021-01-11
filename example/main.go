e898ef3dc3660b3cff3e2514e5bf011deb62a253
Viktor A. Rozenko Voitenko <sharp.vik@gmail.com>
2021-01-11 23:25:09 +0000

    Non-thread-safe version ready

---
+++ example/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"os"
+
+	"github.com/sharpvik/log-go"
+)
+
+func init() {
+	// Change log level.
+	log.SetLevel(log.LvlInfo) // default: LvlError
+
+	// Change log writer.
+	file, _ := os.Create("server.log")
+	log.SetWriter(file) // default: os.Stdout
+}
+
+func main() {
+	// Computations ...
+	x := 40 + 2
+
+	// Print log with priority level Info.
+	log.Info("x = %d", x)
+	// * 11/01/2021 23:07:08 INFO  x = 42
+}
